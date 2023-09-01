ba576798751553776f7431264f78299992847061
Abubakar <Haywayaheadshotstrategy.fx@gmail.com>
2023-09-01 08:12:02 -0500

    Add home page header

--- src/app/hello/page.tsx
+++ src/app/hello/page.tsx
@@ -5,7 +5,12 @@ const Hello = () => {
   return (
     <NavWrapper>
       <div>
-          <h1>Hello World!</h1>
+          <h1>Welcome to Baby Express!</h1>
+          <p>
+            Get quality and affordable for babies and pregnant mothers.
+            Our products are sourced from United States, Turkey and in some cases
+            China.
+          </p>
       </div>
     </NavWrapper>
   )
